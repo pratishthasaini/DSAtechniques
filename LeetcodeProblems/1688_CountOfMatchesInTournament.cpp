0609fad3ee1dd47d73f3b5b211acbc0d25bed636
pratishthasaini <103769329+pratishthasaini@users.noreply.github.com>
2023-12-05 09:28:24 +0530

    1688_CountOfMatchesInTournament.cpp

--- LeetcodeProblems/1688_CountOfMatchesInTournament.cpp
+++ LeetcodeProblems/1688_CountOfMatchesInTournament.cpp
@@ -24,3 +24,26 @@ public:
         return totalMatches;
     }
 };
+
+//or we can use this code optimally
+
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int totalMatches = 0;
+        int totalteams = 0;
+        while(n != 1){
+            if(n%2 == 0){
+                totalMatches += (n/2);
+                totalteams += (n/2);
+                n = n/2;
+            }
+            else{
+                totalMatches += ((n-1)/2);
+                totalteams += (((n-1)/2) + 1);
+                n = ((n-1)/2) + 1;
+            }
+        }
+        return totalMatches;
+    }
+};
