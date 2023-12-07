7dde182282df8ee7479c6555248aea203b97b3a7
pratishthasaini <103769329+pratishthasaini@users.noreply.github.com>
2023-12-07 18:26:13 +0530

    1903_LargestOddNumberInString.cpp

---
+++ LeetcodeProblems/1903_LargestOddNumberInString.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        string ans = "";
+        int n = num.size();
+        for(int i = n-1; i >= 0; i--){
+            if((num[i] - '0') % 2 != 0){
+              //because indexing starts from 0
+                ans = num.substr(0,i+1);
+                break;
+            }
+        }
+        return ans;
+    }
+};
